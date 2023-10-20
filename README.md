cb42050a30543116b820cb28918b627146a13b0f
josephdakotajohnson <josephdakotajohnson@gmail.com>
2023-10-20 12:44:24 -0500

    Created README.md generation

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
-# ReadMe_Generator
-I want to create a generator of ReadMes.
+# testRepoName
+testDescription 
+
+## Description
+
+ - testDescription
+
+## Table of Contents
+
+  - [Credits](#credits)
+  - [Badges](#badges)
+  - [Features](#features)
+  - [Questions](#questions)
+  - [Screenshot](#screenshot)
+  - [DeployedApp](#deployedApp)
+
+## Credits
+
+ - This website was coded by testFullName.
+
+testCredits
+
+## Badges
+
+HTML,CSS,JavaScript
+
+## Features
+
+testWhen I do this. /n testThen I do that! /n testIf I do what?
+
+## Questions
+
+ - If you have any questions for me, my GitHub account is testGitHub.
+
+## Screenshot
+
+ - This is a screenshot of the application: ![Application Screenshot](assets/images/testScreenshotName).
+
+## DeployedApp
+
+ - This is a link to the fully deployed application: (https://josephdakotajohnson.github.io/testRepoName/).
--- index.js
+++ index.js
@@ -1,52 +1,57 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
-const fs = require('fs');
+// const fs = require('fs');
+const { writeFile } = require('fs').promises;
 // TODO: Create an array of questions for user input
-const questions = [
-    {
-        type: 'input',
-        name: 'deployedApp',
-        message: 'What is the name of the repository (this is case sensitive, example: "ReadMe_Generator")?',
-    },
-    {
-        type: 'input',
-        name: 'gitHub',
-        message: `What is the name of your GitHub (type this example: "[SoAndSo's GitHub account](https://github.com/soandso)"?`,
-    },
-    {
-        type: 'input',
-        name: 'description',
-        message: 'What is the description (example: "I want to create...")?',
-    },
-    {
-        type: 'input',
-        name: 'credits1',
-        message: 'Who coded this website?',
-    },
-    {
-        type: 'input',
-        name: 'credits2',
-        message: 'Who should the README credit?',
-    },
-    {
-        type: 'checkbox',
-        name: 'badges',
-        message: 'What badges should be added to the README?',
-    },
-    {
-        type: 'input',
-        name: 'features',
-        message: 'What are the features? (Use "When", "Then", and "If", place " - " before each one and a space after each one.)',
-    },
-    {
-        type: 'input',
-        name: 'screenshot',
-        message: 'What is the name of the screenshot?',
-    },
-];
+const questions = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'deployedApp',
+            message: 'What is the name of the repository (this is case sensitive, example: "ReadMe_Generator")?',
+        },
+        {
+            type: 'input',
+            name: 'gitHub',
+            message: `What is the name of your GitHub (type this example: "[SoAndSo's GitHub account](https://github.com/soandso)"?`,
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'What is the description (example: "I want to create...")?',
+        },
+        {
+            type: 'input',
+            name: 'credits1',
+            message: 'Who coded this website?',
+        },
+        {
+            type: 'input',
+            name: 'credits2',
+            message: 'Who should the README credit?',
+        },
+        {
+            type: 'checkbox',
+            name: 'badges',
+            message: 'What badges should be added to the README?',
+            choices: ['HTML', 'CSS', 'JavaScript'],
+        },
+        {
+            type: 'input',
+            name: 'features',
+            message: 'What are the features? (Use "When", "Then", and "If", place " - " before each one and a space after each one.)',
+        },
+        {
+            type: 'input',
+            name: 'screenshot',
+            message: 'What is the name of the screenshot?',
+        },
+    ])
+};
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {
+// function writeToFile(fileName, data) {
+const generateREADME = ({ deployedApp, gitHub, description, credits1, credits2, badges, features, screenshot }) =>
 `# ${deployedApp}
 ${description} 
 
@@ -71,7 +76,7 @@ ${credits2}
 
 ## Badges
 
- - 
+${badges}
 
 ## Features
 
@@ -88,10 +93,127 @@ ${features}
 ## DeployedApp
 
  - This is a link to the fully deployed application: (https://josephdakotajohnson.github.io/${deployedApp}/).
-`;}
+`;
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    questions()
+        .then((answers) => writeFile('README.md', generateREADME(answers)))
+        .then(() => console.log('Successfully wrote a README.md'))
+        .catch((err) => console.error(err));
+};
 
 // Function call to initialize app
 init();
+
+
+
+
+
+
+
+// // TODO: Include packages needed for this application
+// const inquirer = require('inquirer');
+// const fs = require('fs');
+// // TODO: Create an array of questions for user input
+// inquirer
+//     .prompt([
+//     {
+//         type: 'input',
+//         name: 'deployedApp',
+//         message: 'What is the name of the repository (this is case sensitive, example: "ReadMe_Generator")?',
+//     },
+//     {
+//         type: 'input',
+//         name: 'gitHub',
+//         message: `What is the name of your GitHub (type this example: "[SoAndSo's GitHub account](https://github.com/soandso)"?`,
+//     },
+//     {
+//         type: 'input',
+//         name: 'description',
+//         message: 'What is the description (example: "I want to create...")?',
+//     },
+//     {
+//         type: 'input',
+//         name: 'credits1',
+//         message: 'Who coded this website?',
+//     },
+//     {
+//         type: 'input',
+//         name: 'credits2',
+//         message: 'Who should the README credit?',
+//     },
+//     {
+//         type: 'checkbox',
+//         name: 'badges',
+//         message: 'What badges should be added to the README?',
+//     },
+//     {
+//         type: 'input',
+//         name: 'features',
+//         message: 'What are the features? (Use "When", "Then", and "If", place " - " before each one and a space after each one.)',
+//     },
+//     {
+//         type: 'input',
+//         name: 'screenshot',
+//         message: 'What is the name of the screenshot?',
+//     },
+// ])
+//     .then((answers) => {
+//         const readmePageContent = generateHTML(answers);
+
+//         fs.writeFile('README.md', htmlPageContent, (err) =>
+//         err ? console.log(err) : console.log('Successfully created index.html!')
+//         );
+// });
+
+// // TODO: Create a function to write README file
+// function writeToFile(fileName, data) {
+// `# ${deployedApp}
+// ${description} 
+
+// ## Description
+
+//  - ${description}
+
+// ## Table of Contents
+
+//   - [Credits](#credits)
+//   - [Badges](#badges)
+//   - [Features](#features)
+//   - [Questions](#questions)
+//   - [Screenshot](#screenshot)
+//   - [DeployedApp](#deployedApp)
+
+// ## Credits
+
+//  - This website was coded by ${credits1}.
+
+// ${credits2}
+
+// ## Badges
+
+//  - 
+
+// ## Features
+
+// ${features}
+
+// ## Questions
+
+//  - If you have any questions for me, my GitHub account is ${gitHub}.
+
+// ## Screenshot
+
+//  - This is a screenshot of the application: ![Application Screenshot](assets/images/${screenshot}).
+
+// ## DeployedApp
+
+//  - This is a link to the fully deployed application: (https://josephdakotajohnson.github.io/${deployedApp}/).
+// `;}
+
+// // TODO: Create a function to initialize app
+// function init() {}
+
+// // Function call to initialize app
+// init();
